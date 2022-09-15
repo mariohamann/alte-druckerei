8ded61cf8079df1266b19b89231346f619ad617e
mariohamann <mario.hamann@virtual-identity.com>
2022-09-15 22:45:13 +0200

    fix: make accessible

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
     extend: {
       fontFamily: {
         sans: '"PT Sans", sans-serif'
       }
     },
-	},
-	plugins: [],
+  },
+  plugins: [],
 }
